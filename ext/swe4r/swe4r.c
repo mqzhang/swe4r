4d71b80d06d31c564599698a5f4dbc7b388a8752
David Lowenfels <282+dfl@users.noreply.github.com>
2023-01-26 18:11:33 -0800

    update copyright

--- ext/swe4r/swe4r.c
+++ ext/swe4r/swe4r.c
@@ -1,6 +1,7 @@
 /*
 Swe4r :: Swiss Ephemeris for Ruby - A C extension for the Swiss Ephemeris library (http://www.astro.com/swisseph/)
 Copyright (C) 2012 Andrew Kirk (andrew.kirk@windhorsemedia.com)
+Additional work (C) 2023 David Lowenfels (dfl@alum.mit.edu)
 
 This file is part of Swe4r.
 
@@ -18,8 +19,10 @@ You should have received a copy of the GNU General Public License
 along with Swe4r.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "ruby.h"
+
+// https://docs.ruby-lang.org/en/3.0/extension_rdoc.html#include "ruby.h"
 #include "swephexp.h"
+
 
 // Module Name
 VALUE rb_mSwe4r = Qnil;
